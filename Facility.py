8ea3595c75500426e7b875d20c5b07e23a260429
Michellepotran <113163108+Michellepotran@users.noreply.github.com>
2022-12-09 18:48:16 -0700

    Create Facility.py

---
+++ Facility.py
@@ -0,0 +1,38 @@
+# Defining the facility class
+class Facility:
+    # Creating the constructor for Facility class
+    def __init__(self, fName): # Attributes
+        self.fName = fName
+
+
+# function to display Facilities list
+def displayFacilities():
+    print("The Hospital Facilities are: \n")
+    for i in facilities:
+        print(i.fName)
+
+
+# Function to add new Facility to the list
+def addFacility():
+    list_length = 1
+    for i in range(list_length):
+        fName = input("\nEnter Facility name: ")
+        facilities.append(Facility(fName))
+
+# Function to display the menu
+def facilityMenu():
+    while True:
+        print("\n 1 - Display Facilities list \n 2 - Add Facility \n 3 - Back to the Main Menu \n")
+        category = input()
+
+        if (category == '1'):
+            displayFacilities()
+        elif (category == '2'):
+            addFacility()
+        elif (category == '0'):
+            break
+
+
+facilities = [Facility("Ambulance"), Facility("Admissions"), Facility("Canteen"), Facility("Emergency")]
+# User chooses the category
+facilityMenu()
